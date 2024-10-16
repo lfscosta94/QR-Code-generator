4d488b87e09816d589bff478b56e45c5549bbb4c
lfscosta94 <lfscosta94@gmail.com>
2024-10-16 13:04:25 -0300

    Add files via upload
    Projeto feito através do curso "Hora de Codar", UDEMY

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = document.querySelector("#qr-form input");
+
+// Eventos
+function generateQRCode() {
+    
+    const qrCodeInputValue = qrCodeInput.value
+
+    console.log(qrCodeInputValue);
+}
+
+qrCodeBtn.addEventListener("click", () => {
+
+})
